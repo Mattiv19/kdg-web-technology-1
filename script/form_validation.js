50967bffd5d49945e0ccbc6d247730fa75b0c53f
MatthiasKdG <matthias.vermeiren@student.kdg.be>
2022-12-08 19:50:07 +0100

    JS Customization - M3.1 OK

--- script/form_validation.js
+++ script/form_validation.js
@@ -9,40 +9,54 @@ function init(){
     form.addEventListener('submit', submit, false);
 }
 
-function validateFullName(fullnameField){
+function validateFullName(){
     let content = document.getElementById("fullname").value;
     let feedback = document.getElementById("errorFullName");
+    const validcolor = document.getElementById('fullname');
 
     if(content.trim().length !== content.length){
         console.log(content.trim().length);
         console.log(content.length);
         feedback.innerHTML = "⚠️ Your last name should not contain any spaces before or after.";
-        fullnameField.classList.remove("validvalue");
+        validcolor.classList.remove("validvalue");
         return false;
     } else{
         feedback.innerHTML="";
-        fullnameField.classList.add('validvalue');
+        if(content !== ""){
+        validcolor.classList.add("validvalue");
+        } else{
+            validcolor.classList.remove("validvalue");
+        }
     }
     return true;
 }
 
-function validateEmail(emailField){
+function validateEmail(){
     let mail = document.getElementById("email").value;
     let feedback = document.getElementById("errorMail");
     const validationEmailPattern = /^[A-z\-]+\.[A-z\-]+[.{1}[A-z\-]*@(student.)*kdg.be/;
+    const validcolor = document.getElementById('email');
+
     if(mail.charAt(0) === "." || mail.charAt(mail.length-1) === "."){
         feedback.innerHTML = "⚠️ Your email should not start or end with a dot.";
+        validcolor.classList.remove("validvalue");
         return false;
     } else if(mail.includes("..")){
         feedback.innerHTML = "⚠️ Your email should not contain more than one dot.";
+        validcolor.classList.remove("validvalue");
         return false;
     } else if(!validationEmailPattern.test(mail)){
         console.log(mail)
         feedback.innerHTML = "⚠️ your email should be of type @student.kdg.be or @kdg.be";
+        validcolor.classList.remove("validvalue");
         return false;
     } else{
         feedback.innerHTML = "";
-        emailField.classList.add("validvalue");
+        if(mail !== "") {
+            validcolor.classList.add("validvalue");
+        } else{
+            validcolor.classList.remove("validvalue");
+        }
     }
     return true;
 }
@@ -57,3 +71,4 @@ function submit(event){
         console.log(feedback);
     }
 }
+
